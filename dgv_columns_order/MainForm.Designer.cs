96df3475b1220cb287bb23ade4074d47a8c17c81
Tom Gregor <tgregor@ivsoftware.com>
2022-05-25 07:00:28 -0600

    Add project files.

---
+++ dgv_columns_order/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace dgv_columns_order
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnChangeOrder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.Size = new System.Drawing.Size(800, 393);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // btnChangeOrder
+            // 
+            this.btnChangeOrder.Location = new System.Drawing.Point(536, 404);
+            this.btnChangeOrder.Name = "btnChangeOrder";
+            this.btnChangeOrder.Size = new System.Drawing.Size(217, 34);
+            this.btnChangeOrder.TabIndex = 1;
+            this.btnChangeOrder.Text = "Change Order";
+            this.btnChangeOrder.UseVisualStyleBackColor = true;
+            this.btnChangeOrder.Click += new System.EventHandler(this.btnChangeOrder_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeOrder);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "MainForm";
+            this.Text = "Standard Column Names";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnChangeOrder;
+    }
+}
+
---
+++ dgv_columns_order/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dgv_columns_order
+{
+    enum StdColumnName
+    {
+        Apple0, 
+        Orange1,
+        Banana2,
+    }
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            foreach (string name in Enum.GetNames(typeof(StdColumnName)))
+            {
+                var col = new DataGridViewTextBoxColumn()
+                {
+                    Name = name,
+                    HeaderText = name,
+                    AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill 
+                };
+                dataGridView.Columns.Add(col);
+            }
+        }
+
+        private void btnChangeOrder_Click(object sender, EventArgs e)
+        {
+            var col = dataGridView.Columns[nameof(StdColumnName.Banana2)];
+            col.DisplayIndex = 0;
+        }
+    }
+}
